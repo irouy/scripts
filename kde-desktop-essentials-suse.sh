f192189ac81596f10e1322a32f4086a90ccac9ea
miruoy <yourimatthys@gmail.com>
2015-10-12 23:09:52 +0200

    Update kde-desktop-essentials-suse.sh

--- kde-desktop-essentials-suse.sh
+++ kde-desktop-essentials-suse.sh
@@ -1,1 +1,1 @@
-sudo zypper in git wireshark whois ipcalc chromium audacity zenmap steam quassel-client yakuake breeze-gtk redshift plasmoid-redshift virt-viewer 
+sudo zypper in wireshark whois ipcalc chromium audacity zenmap steam quassel-client yakuake breeze-gtk redshift plasmoid-redshift virt-viewer
