dad84a4b618fa45e3e9c5ce5f53cc61df9aa143b
miruoy <yourimatthys@gmail.com>
2015-10-12 21:59:34 +0200

    compression

--- tar-full-incremental.sh
+++ tar-full-incremental.sh
@@ -3,23 +3,24 @@
 # created 07 February 2000
 # Based on a script by Daniel O'Callaghan <danny@freebsd.org>
 # and modified by Gerhard Mourani <gmourani@videotron.ca>
+# 2015-10-12 yourimatthys [at] gmail [dot] com
 
 #Change the 5 variables below to fit your computer/backup
 
 COMPUTER=                           	# name of this computer
-DIRECTORIES=                   	# directoris to backup
+DIRECTORIES=                   	        # directoris to backup
 BACKUPDIR=                      	# where to store the backups
-TIMEDIR=              	# where to store time of full backup
-TAR=/bin/tar                            		# name and locaction of tar
+TIMEDIR=              	                # where to store time of full backup
+TAR=/bin/tar                            # name and locaction of tar
 
 #You should not have to change anything below here
 
 PATH=/usr/local/bin:/usr/bin:/bin
 DOW=`date +%a`              		# Day of the week e.g. Mon
 DOM=`date +%d`              		# Date of the Month e.g. 27
-DM=`date +%d%b`             	# Date and Month e.g. 27Sep
+DM=`date +%d%b`             	        # Date and Month e.g. 27Sep
 
-# On the 1st of the month a permanet full backup is made
+# On the 1st of the month a permanent full backup is made
 # Every Sunday a full backup is made - overwriting last Sundays backup
 # The rest of the time an incremental backup is made. Each incremental
 # backup overwrites last weeks incremental backup of the same name.
@@ -32,7 +33,7 @@ DM=`date +%d%b`             	# Date and Month e.g. 27Sep
 # Monthly full backup
 if [ $DOM = "01" ]; then
         NEWER=""
-        $TAR $NEWER -cf $BACKUPDIR/$COMPUTER-$DM.tar $DIRECTORIES
+        $TAR $NEWER -czf $BACKUPDIR/$COMPUTER-$DM.tar.gz $DIRECTORIES
 fi
 
 # Weekly full backup
@@ -42,12 +43,12 @@ if [ $DOW = "Sun" ]; then
 
         # Update full backup date
         echo $NOW > $TIMEDIR/$COMPUTER-full-date
-        $TAR $NEWER -cf $BACKUPDIR/$COMPUTER-$DOW.tar $DIRECTORIES
+        $TAR $NEWER -czf $BACKUPDIR/$COMPUTER-$DOW.tar.gz $DIRECTORIES
 
 # Make incremental backup - overwrite last weeks
 else
 
         # Get date of last full backup
         NEWER="--newer `cat $TIMEDIR/$COMPUTER-full-date`"
-        $TAR $NEWER -cf $BACKUPDIR/$COMPUTER-$DOW.tar $DIRECTORIES
+        $TAR $NEWER -czf $BACKUPDIR/$COMPUTER-$DOW.tar.gz $DIRECTORIES
 fi
