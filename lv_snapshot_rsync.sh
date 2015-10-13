0486e857333302d162a5ed86e4ca2b205915e3fd
miruoy <yourimatthys@gmail.com>
2015-10-13 11:13:37 +0200

    Update lv_snapshot_rsync.sh

--- lv_snapshot_rsync.sh
+++ lv_snapshot_rsync.sh
@@ -11,15 +11,29 @@ REMOTEHOST=""
 REMOTEDIR=""
 
 # Make sure only root can run our script
-	if [ "$(id -u)" != "0" ]; then
-		echo "This script must be run as root" 1>&2
-		exit 1
-	fi
+        if [ "$(id -u)" != "0" ]; then
+                echo "This script must be run as root" 1>&2
+                exit 1
+        fi
 
-lvcreate -L $SNAPSIZE -s /dev/$WORKVG/$SOURCELV -n $SNAPLV
-mkdir /mnt/$SNAPLV
+# Check if SNAPLV exists
+        if [[ $(lvs | grep $SNAPLV) ]]; then
+                echo "$SNAPLV already exists! Aborting" 1>&2
+                exit 1
+        else
+                lvcreate -L $SNAPSIZE -s /dev/$WORKVG/$SOURCELV -n $SNAPLV
+        fi
+
+# Make sure $DIRECTORY exists, if not then create directory!
+        if [ -d /mnt/$SNAPLV ]
+        then
+                echo "Directory $SNAPLV exists."
+        else
+                mkdir /mnt/$SNAPLV
+        fi
+
+
 mount $MOUNTOPTS /dev/$WORKVG/$SNAPLV /mnt/$SNAPLV
-
 rsync -av --delete-after /mnt/$SNAPLV/ $REMOTEHOST:$REMOTEDIR/
 
 umount /mnt/$SNAPLV
