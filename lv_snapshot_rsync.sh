d3e62dd5ecb9459daf471845f67d50c6e5ab8356
miruoy <yourimatthys@gmail.com>
2015-12-31 18:26:12 +0100

    Now using rsyncd for proper remote paths

--- lv_snapshot_rsync.sh
+++ lv_snapshot_rsync.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
-# 2015-10-12 yourimatthys [at] gmail [dot] com
+# 2015-10-12 miruoy
+# Now using rsyncd for proper remote paths
 
-WORKVG="VGSYSTEM"
-SOURCELV="LVHOME"
-SNAPLV="LVHOME-SNAP1"
-SNAPSIZE="5G"
-MOUNTOPTS="-onouuid,ro" # << if FS == XFS
-REMOTEHOST=""
-REMOTEDIR=""
+WORKVG="VG_VMS01_DATA"
+SOURCELV="LV_LIBVIRT"
+SNAPLV="LV_LIBVIRT-SNAP1"
+SNAPSIZE="10G"
+MOUNTOPTS="-onouuid,ro" # << if FS == XFS -onouuid,ro
+REMOTEHOST="NAS"
+REMOTEDIR="BACKUP_VMS01/libvirt"
 
 # Make sure only root can run our script
         if [ "$(id -u)" != "0" ]; then
@@ -34,8 +35,7 @@ REMOTEDIR=""
 
 
 mount $MOUNTOPTS /dev/$WORKVG/$SNAPLV /mnt/$SNAPLV
-rsync -av --delete-after /mnt/$SNAPLV/ $REMOTEHOST:$REMOTEDIR/
-
+rsync -av --delete-after /mnt/$SNAPLV/ $REMOTEHOST::$REMOTEDIR
 umount /mnt/$SNAPLV
 rmdir /mnt/$SNAPLV
 lvremove -f /dev/$WORKVG/$SNAPLV
