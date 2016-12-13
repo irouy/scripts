661457bb4b3dd44f91162b73f9e0027f3b85ddca
miruoy <yourimatthys@gmail.com>
2016-12-13 11:05:18 +0100

    Update option43siemens.php

--- tools/option43siemens.php
+++ tools/option43siemens.php
@@ -1,38 +1,6 @@
  <?php echo '<p>optie43 converter mash-up 20161212 </p>'; ?> 
- <?php echo '<p>form input example code</p>'; ?>
-<form action="" method="post">
-HEX: <input type="text" name="HEX_INPUT"><br>
-<input type="submit">
-</form>
-<form action="" method="post">
-ASCII: <input type="text" name="ASCII_INPUT"><br>
-<input type="submit">
-</form>
- <?php echo '<p>=========================</p>'; ?>
- <?php echo '<p>hex2bin function example code</p>'; ?>
-<?php
-$hex = hex2bin("766C616E");
-var_dump($hex);
-?>
-<?php
-if ( !function_exists( 'hex2bin' ) ) {
-    function hex2bin( $str ) {
-        $sbin = "";
-        $len = strlen( $str );
-        for ( $i = 0; $i < $len; $i += 2 ) {
-            $sbin .= pack( "H*", substr( $str, $i, 2 ) );
-        }
-
-        return $sbin;
-    }
-}
-?>
- <?php echo '<p>=========================</p>'; ?>
- <?php echo '<p>hex2str	str2hex function example code</p>'; ?>
+ 
 <?php 
-print hextostr("73646c703a2f2f3031302e3032302e3030312e3031313a3138343433")."\n"; 
-
-print strtohex("sdlp://010.020.001.011:18443")."\n"; 
 
 function hextostr($x) { 
   $s=''; 
@@ -46,4 +14,27 @@ function strtohex($x) {
   return($s); 
 } 
 ?>
- <?php echo '<p>=========================</p>'; ?>
+
+<form action="" method="post">
+	HEX: <input type="text" name="HEX_INPUT"><br>
+	<input type="submit">
+</form>
+
+<form action="" method="post">
+	ASCII: <input type="text" name="ASCII_INPUT"><br>
+	<input type="submit">
+</form>
+
+<?php
+
+//if (isset($_POST['HEX_INPUT'})) {
+//	print hextostr($_POST['HEX_INPUT']);
+//	}
+//else {
+//	print_form();
+//}
+	
+print hextostr("73646c703a2f2f3031302e3032302e3030312e3031313a3138343433")."\n"; 
+
+print strtohex("sdlp://010.020.001.011:18443")."\n"; 
+?>
