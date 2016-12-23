d6120f997935c81d077ec2b596977a46e58dfd47
miruoy <yourimatthys@gmail.com>
2016-12-23 12:40:10 +0100

    a

---
+++ tools/crontab_bootstrap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+TMPFILE="/tmp/bootstrap_$USER.cronout"
+
+#Check if we are in the right directory.
+if [ -e $PWD/crontab_bootstrap.sh ]; then printf "\nValidated directory, continuing routine."; 
+	else printf "\nYou must run this script from its directory!"
+		printf "\nAborting!"
+		exit
+fi
+
+printf "\nPlease input alerting email address [email@example.com]\n"
+	read ALERTMAILADDR
+	ls -lah /var && printf "\nPlease input Backup Path [/var/backup]\n" && read BACKDIR
+	printf "\nEmail address for alerting == $ALERTMAILADDR"
+	printf "\nPath that will be used for storing backup files == $BACKDIR"
+	printf "\nIs this correct? [y/N]\n"
+		read -n 1 CONTINUE
+			if [ $CONTINUE == "y" ]; then
+			printf "Email and backup path validated, continuing routine."
+			else
+		printf "\nAborting!\n"
+		exit
+		fi  
+	
+# Setup CRON
+crontab -l > $TMPFILE && printf "\nLoaded current crontab into $TMPFILE\n" && printf "\nOriginal Crontab\n" && cat $TMPFILE 
+
+	echo "0 * * * * $PWD/diskcheck.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/diskcheck.sh
+	echo "10 0 * * * /bin/tar cvpf $BACKDIR/$HOSTNAME-$USER-daily.tar $HOME | mail -s '$HOSTNAME Daily $USER $HOME TAR' $ALERTMAILADDR" >> $TMPFILE
+	echo "20 0 * * * /usr/bin/crontab -l > $BACKDIR/$HOSTNAME-crontab-$USER-daily.crontab | mail -s '$HOSTNAME Backup crontab $USER daily' $ALERTMAILADDR" >> $TMPFILE
+	echo "30 0 * * * /bin/tar cvpf $BACKDIR/$HOSTNAME-etc-daily.tar /etc |mail -s '$HOSTNAME Backup etc' $ALERTMAILADDR" >> $TMPFILE
+
+printf "\nInstall KVM_BACKUP_LVM scripts? [y/N]"
+	read -n 1 CONTINUE
+		if [ $CONTINUE == "y" ]; then
+			       	
+			       	if [[ $USER != "root" ]]; then 
+                printf "\nThis script must be run as root!\n" 
+                exit 1
+        				fi
+        		
+        		vgs && printf "\nPlease input Volume Group Name\n" && read VOLGRP
+				lvs && printf "\nPlease input Logical Volume Name\n" && read LVOLUME
+				blkid && printf "\nPlease input additional options for mounting Snapshot [-onouuid,ro]\n" && read MOUNTOPTS
+				printf "\nPlease input Snapshot size [16G]\n" && read SNAPSIZE
+			
+			printf "\n Will process $VOLGRP/$LVOLUME to $BACKDIR with Snapsize of $SNAPSIZE and mount options $MOUNTOPTS"
+			printf "\nIs this correct? [y/N]\n"
+				read -n 1 CONTINUE
+						if [ $CONTINUE == "y" ]; then
+							echo "15 2 * * * $PWD/../backup/KVM_BACKUP_LVM.sh $VOLGRP $LVOLUME $BACKDIR $MOUNTOPTS $SNAPSIZE |mail -s '$HOSTNAME Backup $VOLGRP/$LVOLUME to $BACKDIR' $ALERTMAILADDR " >> $TMPFILE
+						fi
+		else
+		printf "\nSkipping KVM_BACKUP_LVM scripts."
+		fi
+
+crontab $TMPFILE && printf "\nInstalled new Crontab\n" && crontab -l
+exit
--- tools/dotfiles_bootstrap.sh
+++ tools/dotfiles_bootstrap.sh
@@ -4,11 +4,11 @@ CONFDIR="../config"
 # Link dotfiles into home folder
 printf "Soft-linking dotfiles in $PWD/$CONFDIR to $HOME" 
 
-ln -s $PWD/$CONFDIR/.bashrc $HOME/.bashrc
-ln -s $PWD/$CONFDIR/.alias $HOME/.alias && sh $HOME/.bashrc
-ln -s $PWD/$CONFDIR/.screenrc $HOME/.screenrc
-ln -s $PWD/$CONFDIR/.vimrc $HOME/.vimrc
-ln -s $PWD/$CONFDIR/.guake.autostart $HOME/.guake.autostart && chmod +x $HOME/.guake.autostart
+ln -s $PWD/$CONFDIR/.bashrc $HOME/.bashrc && printf "\nLinked .bashrc"
+ln -s $PWD/$CONFDIR/.alias $HOME/.alias && sh $HOME/.bashrc && printf "\nLinked .alias and Sourced $HOME/.bashrc"
+ln -s $PWD/$CONFDIR/.screenrc $HOME/.screenrc && printf "\nLinked .screenrc"
+ln -s $PWD/$CONFDIR/.vimrc $HOME/.vimrc && printf "\nLinked .vimrc"
+ln -s $PWD/$CONFDIR/.guake.autostart $HOME/.guake.autostart && chmod +x $HOME/.guake.autostart && printf "\n:Linked .guake.autostart"
 
 printf "\nInstall Conky for GNOME3, XFCE or none? [g/x/n]"
 	read -n 1 CONKYDE
@@ -18,26 +18,12 @@ printf "\nInstall Conky for GNOME3, XFCE or none? [g/x/n]"
 		elif [ $CONKYDE == "x" ]; then
 			printf "\nInstalling XFCE version"
 			ln -s $PWD/$CONFDIR/.conkyrc_xfce3 $HOME/.conkyrc
-		elif [ $CONKYDE == "n" ]; then
-			printf "\nSkipping .conkyrc"
 	else 
 		printf "\nSkipping .conkyrc"
 fi
 
-# Setup CRON
-
-if [ $(ls /tmp/miruoy.dotfiles.bootstrap.cronout) != 1 ]; then
-	printf "cronfile exists"
-	exit
-	else
-	printf "cronfile does exist"
-	exit
-	fi
-
-#crontab -l > /tmp/bootstrap.cronout
-printf "\nPlease input alerting email address [email@example.com]"
-	read ALERTMAILADDR
-	echo "\n mail addr for alerting == $ALERTMAILADDR"
+printf "\nDotfiles succesfully deployed into $HOME"
+printf "\nHave a lot of fun!"
 
 
 exit
