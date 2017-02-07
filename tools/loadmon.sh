9454cfdc325cf8af8b12afb39f69821f5566a351
root <root@DVOHQ-VMS02.DAVOCOM.local>
2017-02-07 08:17:31 +0100

    modified:   loadmon.sh

--- tools/loadmon.sh
+++ tools/loadmon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 trigger=$(/usr/bin/grep pro /proc/cpuinfo -c)
-load=`cat /proc/loadavg | awk '{print $1}'`
+load=`cat /proc/loadavg | awk '{print $3}'`
 response=`echo | awk -v T=$trigger -v L=$load 'BEGIN{if ( L > T){ print "greater"}}'`
 if [[ $response = "greater" ]]
 then
