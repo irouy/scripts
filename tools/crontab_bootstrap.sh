ece4261c47620da7947d91feeb89ba9f15a8e43d
miruoy <yourimatthys@gmail.com>
2017-07-12 00:42:50 +0200

    DRAFT!!!

--- tools/crontab_bootstrap.sh
+++ tools/crontab_bootstrap.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
+
+#backup current crontab
+crontab -l > $HOME/CRONTAB_BOOTSTRAP_BACKUP_$(date +%Y%m%d_%H%M%S)
+
 TMPFILE="/tmp/bootstrap_$USER.cronout"
 
 #Check if we are in the right directory.
-if [ -e $PWD/crontab_bootstrap.sh ]; then printf "\nValidated directory, continuing routine."; 
+if [ -e $PWD/crontab_bootstrap.sh ]
+	then printf "\nValidated directory, continuing routine."; 
 	else printf "\nYou must run this script from its directory!"
 		printf "\nAborting!\n"
-		exit
+	exit
 fi
 
-printf "\nPlease input alerting email address [email@example.com]\n" && read ALERTMAILADDR
-	ls -lah /var && printf "\nPlease input Backup Path [/var/backup]\n" && read BACKDIR
+printf "\nPlease input alerting email address [email@example.com]\n"
+	read ALERTMAILADDR
+ls -lah /var
+	printf "\nPlease input Backup Path [/var/backup]\n"
+		read BACKDIR
+	
 	printf "\nEmail address for alerting == $ALERTMAILADDR"
 	printf "\nPath that will be used for storing backup files == $BACKDIR"
+	
 	printf "\nIs this correct? [y/N]\n"
 		read -n 1 CONTINUE
 			if [ $CONTINUE == "y" ]; then
@@ -19,29 +29,34 @@ printf "\nPlease input alerting email address [email@example.com]\n" && read ALE
 			else
 		printf "\nAborting!\n"
 		exit
-		fi  
-	
-# Setup CRON
+		fi
+		
+	printf "\nInstall KVM_BACKUP_LVM scripts? [y/N]"
+		read -n 1 CONTINUE
+			if [ $CONTINUE == "y" ]; then
+			if [ $CONTINUE == "n" ]; then
+				# Setup CRON
 crontab -l > $TMPFILE && printf "\nLoaded current crontab into $TMPFILE\n" && printf "\nOriginal Crontab\n" && cat $TMPFILE 
-	echo "#DOTFILES.SH $(date)" >> $TMPFILE
+	echo "#DOTFILES.SH $(date +%Y%m%d_%H%M%S)" >> $TMPFILE
 	echo "0 * * * * $PWD/diskcheck.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/diskcheck.sh
 	echo "0 * * * * $PWD/loadmon.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/loadmon.sh
 	echo "10 0 * * * /bin/tar cvpf $BACKDIR/$HOSTNAME-$USER-daily.tar $HOME | mail -s '$HOSTNAME Daily $USER $HOME TAR' $ALERTMAILADDR" >> $TMPFILE
 	echo "20 0 * * * /usr/bin/crontab -l > $BACKDIR/$HOSTNAME-crontab-$USER-daily.crontab | mail -s '$HOSTNAME Backup crontab $USER daily' $ALERTMAILADDR" >> $TMPFILE
 	echo "30 0 * * * /bin/tar cvpf $BACKDIR/$HOSTNAME-etc-daily.tar /etc |mail -s '$HOSTNAME Backup etc' $ALERTMAILADDR" >> $TMPFILE
 
-printf "\nInstall KVM_BACKUP_LVM scripts? [y/N]"
-	read -n 1 CONTINUE
-		if [ $CONTINUE == "y" ]; then
-			       	
+		printf "\nSkipped KVM_BACKUP_LVM scripts but installed basic crontab."
+		fi
 			       	if [[ $USER != "root" ]]; then 
-                printf "\nThis script must be run as root!\n" 
+                	printf "\nThis script must be run as root!\n" 
                 exit 1
         				fi
-        		
-        		vgs && printf "\nPlease input Volume Group Name\n" && read VOLGRP
-				lvs && printf "\nPlease input Logical Volume Name\n" && read LVOLUME
-				blkid && printf "\nPlease input additional options for mounting Snapshot [-onouuid,ro]\n" && read MOUNTOPTS
+
+        		vgs
+				printf "\nPlease input Volume Group Name\n" && read VOLGRP
+			lvs
+				printf "\nPlease input Logical Volume Name\n" && read LVOLUME
+			blkid
+				printf "\nPlease input additional options for mounting Snapshot [-onouuid,ro]\n" && read MOUNTOPTS
 				printf "\nPlease input Snapshot size [16G]\n" && read SNAPSIZE
 			
 			printf "\n Will process $VOLGRP/$LVOLUME to $BACKDIR with Snapsize of $SNAPSIZE and mount options $MOUNTOPTS"
@@ -50,9 +65,8 @@ printf "\nInstall KVM_BACKUP_LVM scripts? [y/N]"
 						if [ $CONTINUE == "y" ]; then
 							echo "15 2 * * * $PWD/../backup/KVM_BACKUP_LVM.sh $VOLGRP $LVOLUME $BACKDIR $MOUNTOPTS $SNAPSIZE |mail -s '$HOSTNAME Backup $VOLGRP/$LVOLUME to $BACKDIR' $ALERTMAILADDR " >> $TMPFILE
 						fi
-		else
-		printf "\nSkipping KVM_BACKUP_LVM scripts."
-		fi
+
 
 crontab $TMPFILE && printf "\nInstalled new Crontab\n" && crontab -l
+cd $HOME &&  
 exit
