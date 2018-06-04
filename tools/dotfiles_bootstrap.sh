28ef802d35fd91960c8726483419395a642f49bc
miruoy <yourimatthys@gmail.com>
2018-06-04 13:54:42 +0200

    Update dotfiles_bootstrap.sh

--- tools/dotfiles_bootstrap.sh
+++ tools/dotfiles_bootstrap.sh
@@ -6,7 +6,7 @@ if [ -e $PWD/dotfiles_bootstrap.sh ]
 	then printf "\nValidated directory, continuing routine."; 
 	else printf "\nYou must run this script from its directory!"
 		printf "\nAborting!\n"
-		exit
+	exit
 fi
 
 printf "\nClean existing dotfiles in $HOME? [y/N]"
@@ -23,21 +23,18 @@ printf "\nClean existing dotfiles in $HOME? [y/N]"
 fi
 
 # Link dotfiles into home folder
-printf "Soft-linking dotfiles in $PWD/$CONFDIR to $HOME\n" 
+printf "Simlinking dotfiles in $PWD/$CONFDIR to $HOME\n" 
 
 ln -s $PWD/../config/ssh_config $HOME/.ssh/config && "n\Linked ssh_config"
 ln -s $PWD/$CONFDIR/.bashrc $HOME/.bashrc && printf "\nLinked .bashrc"
 ln -s $PWD/$CONFDIR/.alias $HOME/.alias && sh $HOME/.bashrc && printf "\nLinked .alias and Sourced $HOME/.bashrc"
 ln -s $PWD/$CONFDIR/.screenrc $HOME/.screenrc && printf "\nLinked .screenrc"
 ln -s $PWD/$CONFDIR/.vimrc $HOME/.vimrc && printf "\nLinked .vimrc"
-ln -s $PWD/$CONFDIR/.guake.autostart $HOME/.guake.autostart && chmod +x $HOME/.guake.autostart && printf "\n:Linked .guake.autostart"
+
+# Autostart applications
 mkdir $HOME/.config/autostart/ 
 ln -s $PWD/../config/conky.desktop $HOME/.config/autostart/ && printf "\nLinked conky for autostart"
-ln -s /usr/share/applications/guake.desktop $HOME/.config/autostart/ && printf "\nLinked guake for autostart"
 ln -s $PWD/$CONFDIR/.conkyrc_gnome3 $HOME/.conkyrc && printf "\nInstalling Conky GNOME3 version"
-
-cd $HOME
-source $HOME/.bashrc && printf "\Sourced $HOME/.bashrc"
 
 printf "\nDotfiles succesfully deployed into $HOME"
 printf "\nHave a lot of fun!\n"
