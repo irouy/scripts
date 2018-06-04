bbf72a435e66b64783cc57a05a8e08e738eb1f5c
miruoy <yourimatthys@gmail.com>
2018-06-04 14:54:17 +0200

    Update crontab_bootstrap.sh

--- tools/crontab_bootstrap.sh
+++ tools/crontab_bootstrap.sh
@@ -2,57 +2,74 @@
 TMPFILE="/tmp/bootstrap_$USER.cronout"
 
 #Check if we are in the right directory.
-if [ -e $PWD/crontab_bootstrap.sh ]; then printf "\nValidated directory, continuing routine."; 
-	else printf "\nYou must run this script from its directory!"
-		printf "\nAborting!\n"
-		exit
+if [ -e $PWD/crontab_bootstrap.sh ]; then 
+                printf "\nValidated directory, continuing routine."; 
+        else 
+                printf "\nYou must run this script from its directory!"
+                printf "\nAborting!\n"
+        exit
 fi
 
-printf "\nPlease input alerting email address [email@example.com]\n" && read ALERTMAILADDR
-	ls -lah /var && printf "\nPlease input Backup Path [/var/backup]\n" && read BACKDIR
-	printf "\nEmail address for alerting == $ALERTMAILADDR"
-	printf "\nPath that will be used for storing backup files == $BACKDIR"
-	printf "\nIs this correct? [y/N]\n"
-		read -n 1 CONTINUE
-			if [ $CONTINUE == "y" ]; then
-			printf "Email and backup path validated, continuing routine."
-			else
-		printf "\nAborting!\n"
-		exit
-		fi  
-	
+printf "\nPlease input alerting email address [email@example.com]\n" 
+        read ALERTMAILADDR
+ls -lah /var && printf "\nPlease input Backup Path [/var/backup]\n" 
+        read BACKDIR
+
+printf "\nEmail address for alerting == $ALERTMAILADDR"
+printf "\nPath that will be used for storing backup files == $BACKDIR"
+printf "\nIs this correct? [y/N]\n"
+
+read -n 1 CONTINUE
+        if [ $CONTINUE == "y" ]; then
+                printf "Email and backup path validated, continuing routine."
+        else
+                printf "\nAborting!\n"
+        exit
+                fi  
+
+if [ ! -f /root/bin/vm-backup.sh ]; then
+        cp ../backup/vm-backup.sh /root/bin/vm-backup.sh && chmod +x /root/bin/vm-backup.sh
+                printf "\nCopied vm-backup.sh to /root/bin\n"
+        else
+                printf "\nvm-backup.sh already exists in /root/bin\n"
+        fi
+  
 # Setup CRON
 crontab -l > $TMPFILE && printf "\nLoaded current crontab into $TMPFILE\n" && printf "\nOriginal Crontab\n" && cat $TMPFILE 
-	echo "#DOTFILES.SH $(date)" >> $TMPFILE
-	echo "0 * * * * $PWD/diskcheck.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/diskcheck.sh
-	echo "0 * * * * $PWD/loadmon.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/loadmon.sh
-	echo "10 0 * * * /bin/tar cvpf $BACKDIR/$HOSTNAME-$USER-daily.tar $HOME | mail -s '$HOSTNAME Daily $USER $HOME TAR' $ALERTMAILADDR" >> $TMPFILE
-	echo "20 0 * * * /usr/bin/crontab -l > $BACKDIR/$HOSTNAME-crontab-$USER-daily.crontab | mail -s '$HOSTNAME Backup crontab $USER daily' $ALERTMAILADDR" >> $TMPFILE
-	echo "30 0 * * * /bin/tar cvpf $BACKDIR/$HOSTNAME-etc-daily.tar /etc |mail -s '$HOSTNAME Backup etc' $ALERTMAILADDR" >> $TMPFILE
+        echo "#DOTFILES.SH $(date)" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "# set VARIABLES  #" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin" >> $TMPFILE
+        echo "MAILTO="$ALERTMAILADDR"" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "# Examples        #" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "# .---------------- minute (0 - 59)" >> $TMPFILE
+        echo "# |  .------------- hour (0 - 23)" >> $TMPFILE
+        echo "# |  |  .---------- day of month (1 - 31)" >> $TMPFILE
+        echo "# |  |  |  .------- month (1 - 12) OR jan,feb,mar,apr ..." >> $TMPFILE
+        echo "# |  |  |  |  .---- day of week (0 - 6) (Sunday=0 or 7)  OR sun,mon,tue,wed,thu,fri,sat" >> $TMPFILE
+        echo "# |  |  |  |  |" >> $TMPFILE
+        echo "# *  *  *  *  *  command to be executed" >> $TMPFILE
+        echo "# *  *  *  *  *  command --arg1 --arg2 file1 file2 2>&1" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "# HW Checks       #" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "0 * * * * $PWD/diskcheck.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/diskcheck.sh
+        echo "0 * * * * $PWD/loadmon.sh $ALERTMAILADDR >/dev/null 2>&1" >> $TMPFILE && chmod +x $PWD/loadmon.sh
+        echo "#=================" >> $TMPFILE
+        echo "# Config Backup   #" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "0 0 * * * /bin/tar cvzpf $BACKDIR/$HOSTNAME-config-\$(date +\%A).tar.gz /etc /var/spool /root" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "# VM-Backups      #" >> $TMPFILE
+        echo "#=================" >> $TMPFILE
+        echo "#30 0 * * * /root/bin/vm-backup.sh $BACKDIR DAVO-BOOSTER 2" >> $TMPFILE
 
-printf "\nInstall KVM_BACKUP_LVM scripts? [y/N]"
-	read -n 1 CONTINUE
-		if [ $CONTINUE == "y" ]; then
-			       	
-			       	if [[ $USER != "root" ]]; then 
-                printf "\nThis script must be run as root!\n" 
-                exit 1
-        				fi
-        		
-        		vgs && printf "\nPlease input Volume Group Name\n" && read VOLGRP
-				lvs && printf "\nPlease input Logical Volume Name\n" && read LVOLUME
-				blkid && printf "\nPlease input additional options for mounting Snapshot [-onouuid,ro]\n" && read MOUNTOPTS
-				printf "\nPlease input Snapshot size [16G]\n" && read SNAPSIZE
-			
-			printf "\n Will process $VOLGRP/$LVOLUME to $BACKDIR with Snapsize of $SNAPSIZE and mount options $MOUNTOPTS"
-			printf "\nIs this correct? [y/N]\n"
-				read -n 1 CONTINUE
-						if [ $CONTINUE == "y" ]; then
-							echo "15 2 * * * $PWD/../backup/KVM_BACKUP_LVM.sh $VOLGRP $LVOLUME $BACKDIR $MOUNTOPTS $SNAPSIZE |mail -s '$HOSTNAME Backup $VOLGRP/$LVOLUME to $BACKDIR' $ALERTMAILADDR " >> $TMPFILE
-						fi
-		else
-		printf "\nSkipping KVM_BACKUP_LVM scripts."
-		fi
+crontab $TMPFILE
+        printf "\nInstalled new Crontab\n" && crontab -l
+rm $TMPFILE
+        printf "\nCleaned $TMPFILE\n" 
 
-crontab $TMPFILE && printf "\nInstalled new Crontab\n" && crontab -l
 exit
